c20273850fc79ced05f3158cd638252921c592ad
Gustavo Galote <gusgalot3g@gmail.com>
2022-05-27 16:47:41 -0300

    fix: add required to input

--- exercises/exercise-01.php
+++ exercises/exercise-01.php
@@ -17,15 +17,15 @@ include __DIR__ . '/../includes/head.php';
     <form method="post" class="container-sm">
         <div class="mb-3 customBg">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" name="name" value="<?= $_POST['name'] ?>">
+            <input type="text" class="form-control" id="name" name="name" value="<?= $_POST['name'] ?>" required>
         </div>
         <div class="mb-3">
             <label for="weight" class="form-label">Weight</label>
-            <input type="number" class="form-control" id="weight" name="weight" value="<?= $_POST['weight'] ?>">
+            <input type="number" class="form-control" id="weight" name="weight" value="<?= $_POST['weight'] ?>" required>
         </div>
         <div class="mb-3">
             <label for="height" class="form-label">Height</label>
-            <input type="number" class="form-control" id="height" name="height" value="<?= $_POST['height'] ?>"
+            <input type="number" class="form-control" id="height" name="height" value="<?= $_POST['height'] ?>" required>
         </div>
         <div class="w-100 d-flex justify-content-end">
             <button type="submit" class="mt-4 btn btn-primary">Submit</button>
