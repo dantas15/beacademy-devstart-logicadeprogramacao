06cd4ad25022303d2987a7ed1df221bcd1cf6fe6
Gustavo Galote <gusgalot3g@gmail.com>
2022-05-27 16:10:08 -0300

    fix: display exercise title in exercise-details

--- includes/exercise-details.php
+++ includes/exercise-details.php
@@ -1,13 +1,13 @@
-<h1 class="mt-4 mb-2"><?= $exercise->title ?></h1>
+<h1 class="mt-4 mb-2">Exercise details</h1>
 
 <hr/>
 
 <article class="card bg-dark text-bg-dark">
     <div class="card-body">
-        <h2 class="card-title">Card title</h2>
+        <h2 class="card-title"><?= $exercise->title ?></h2>
         <p class="card-text"><?= $exercise->description ?></p>
     </div>
     <div class="card-body">
-        <a href="exercise/<?= $exercise->title ?>.php" class="card-link">Open exercise</a>
+        <a href="exercises/<?= $exercise->title ?>.php" class="card-link">Open exercise</a>
     </div>
 </article>
